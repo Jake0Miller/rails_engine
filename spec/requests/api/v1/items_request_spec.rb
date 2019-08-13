89935b7d7267781bd08e2720235c21ba2672302d
Jake Miller <jake0miller@gmail.com>
2019-08-13 11:30:29 -0600

    Add tests

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Items API' do
+  before :each do
+    @merchant = create(:merchant)
+  end
+
+  it 'sends a list of items' do
+    create_list(:item, 3, merchant: @merchant)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body)["data"]
+
+    expect(items.count).to eq(3)
+  end
+
+  it 'can get one item by its id' do
+    id = create(:item, merchant: @merchant).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body)["data"]
+    binding.pry
+    expect(response).to be_successful
+    expect(item["id"].to_i).to eq(id)
+  end
+end
