b4cf7bc15761be163e875baea5181a541d9799f6
Jake Miller <jake0miller@gmail.com>
2019-08-15 00:03:28 -0600

    Write method to return customers favorite merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,6 +12,16 @@ class Merchant < ApplicationRecord
     joins(:items)
       .where(items: {id: inv_id})
   end
+
+  def self.fav_for_customer(cust_id)
+    test = joins(invoices: :transactions)
+      .where(invoices: {customer_id: cust_id})
+      .merge(Transaction.successful)
+      .select('merchants.*, COUNT(*) AS total')
+      .group(:id)
+      .order(total: :desc)
+      .limit(1)
+  end
 end
 
 # "2012-03-27".to_date.all_day
